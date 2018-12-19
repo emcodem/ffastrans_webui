3d8e073003c623f45a545c3449c90b4d6646adb4
emcodem <emcodem@github.com>
2018-12-19 22:21:49 +0100

    Finish Browse Location config

--- server_config.js
+++ server_config.js
@@ -10,8 +10,8 @@ config.uploadpath = "\\\\localhost\\c$\\temp\\";
 //escape backslash with a backslash, so \\server\folder is written as \\\\server\\folder
 //your path MUST end with slash or backslash, the rest of the code assumes it.
 //display_names and the locations themselves have to contain the same amount of items!
-config.allowed_browse_locations_display_names = ["TEMP","C"];
-config.allowed_browse_locations = ["\\\\localhost\\c$\\temp\\","c:\\"];
+config.allowed_browse_locations_display_names = ["Local examle","UNC example"];
+config.allowed_browse_locations = ["c:\\","\\\\localhost\\c$\\"];
 
 //not yet active, files in upload dir are deleted if older than x hours check/delete happens everytime a new file is uploaded...
 config.uploaded_files_maxage = 1;
