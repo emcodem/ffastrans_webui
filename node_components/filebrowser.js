5be67d48b7348ac92155ffa264a0f41355d09225
emcodem <emcodem@github.com>
2018-12-20 17:34:12 +0100

    Finished rework job submit and workflow listing

--- node_components/filebrowser.js
+++ node_components/filebrowser.js
@@ -15,20 +15,24 @@ module.exports = function(app, express){
 				if ((!baseFolder.endsWith("/")) && (!baseFolder.endsWith("\\"))){
 					baseFolder += "\\";
 				}
-                checkFolderInGlobalConfig(baseFolder); //ends request with error if basefolder is is not within configured locations
-                
-                //start output
-				res.writeHead(200,{"Content-Type" : "application/JSON"});
-				//serve default directory
+                if (!checkFolderInGlobalConfig(baseFolder,res)){
+                   return; 
+                }; //ends request with error if basefolder is is not within configured locations
+
 				var rows = {};
 				rows.rows =[];
-				
-				if (!baseFolder){baseFolder = 'c:\\';};//todo: handle no basepath error
+				if (!baseFolder){throw "Parameter name not specified"};//todo: handle no basepath error
 				var split = baseFolder.split("\\");//todo: make sure path contains only backslashes
 				var parentPath = split.slice(0, split.length - 2).join("\\") + "\\";
 				rows.rows.push({id:"oneup",data:["..",parentPath, true, 0]});//add parent folder
 				fs.readdir(baseFolder, (err, files) => {
-					
+                    if (!files){
+                        res.status(500);//Send error response here
+                        res.send("ERROR: could not list files in " + baseFolder);
+                        res.end();
+                        return;//todo: get out how to catch error of nested 
+                    }
+                    
 				  files.forEach(filename => {
 					  try{//ignore non accessible files and folders
 						const stats = fs.statSync(baseFolder + filename)
@@ -37,18 +41,21 @@ module.exports = function(app, express){
 						  console.log("WARNING: cannot stat file: " + baseFolder + filename +" , Exception: " + exce);
 					  }
 				  });
+                  res.writeHead(200,{"Content-Type" : "application/JSON"});
 				  res.write(JSON.stringify(rows));//output dhtmlx compatible json
 				  res.end();
+                  
 				})		
 			}
 		}catch (ex){
-				console.log("ERROR: unxepected error in filebrowser.js: " + ex.stacktrace)            ;
+				console.log("ERROR: in filebrowser : " + ex.stacktrace);
                 res.status(500);//Send error response here
+                res.send("ERROR: unxepected error in filebrowser: " + ex);
                 res.end();
 		}
 	});
 	
-    function checkFolderInGlobalConfig(folder){
+    function checkFolderInGlobalConfig(folder,res){
         //checks if global conf allows changing into this folder
         var allowed = false;
         for (i=0;i<global.config.allowed_browse_locations.length;i++){
@@ -61,7 +68,8 @@ module.exports = function(app, express){
             return true;
         }else {
             console.log("Directory not allowed: " + folder);
-            res.status(500);//Send error response here
+            res.status(404);//Send error response here
+            res.send("Browsing \""+folder+"\" is not allowed");
             res.end();
         }
         
