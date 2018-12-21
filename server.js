c05dacda225e050119a75d0422ae212e390377db
emcodem <emcodem@github.com>
2018-12-21 13:49:30 +0100

    Bugfix, add ffastranslogo

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 var path = require("path");
 const util = require('util')
-require('console-stamp')(console, '[HH:MM:ss.l]');
 var bodyParser = require('body-parser')
-
+var proxy = require('express-http-proxy');
+require('console-stamp')(console, '[HH:MM:ss.l]');  //adds HHMMss to every console log 
 
 //needed for running as nexe
 var currentFullpath = path.dirname(process.execPath);
@@ -18,6 +18,13 @@ try{
     global.config = require(__dirname + '/server_config');
     console.log("Running as node script")
 }
+
+//log any request
+app.use(function(req, res, next) {
+    //console.log(req.originalUrl);
+    next();
+});
+
 //redirect user config
 /*app.get('/', function(req, res,next) {
     
@@ -28,9 +35,12 @@ try{
     //res.end();
 });*/
 
-
+//needed to parse parameters from requests
 app.use(bodyParser.urlencoded({ extended: true }));
- 
+
+//forward requests to ffastrans # export variable for debugging: set DEBUG=express-http-proxy (onwindows)
+app.use('/proxy', proxy('http://localhost:65445'));//TODO: read ffastrans server and port from config
+
 // serve static websites
 app.use(express.static('./'));
 app.use(express.static(__dirname + '/webinterface'));
@@ -48,12 +58,10 @@ process.on('uncaughtException', function(err) {
   console.log('Caught exception: ' + err);
 });
 
-
 //favicon
 app.use('/favicon.ico', express.static('webinterface/images/favicon.ico'));
 
-
-
+//startup
 console.log('Hello and welcome, thank you for using FFAStrans') 
 app.listen(global.config.port, () => console.log('Running on http://localhost:' + global.config.port));
 
