2d9b745ed1b25ee995a5f43ecbe82b6b72606cd6
emcodem <emcodem@github.com>
2019-01-12 22:15:00 +0100

    Store/restore all user layouts

--- server.js
+++ server.js
@@ -152,6 +152,7 @@ function init(conf){
     require("./node_components/views/usergrouplist")(app, express);
     require("./node_components/views/usergrouprightslist")(app, express);
     require("./node_components/get_userpermissions")(app, passport);
+    require("./node_components/resumeable_backend.js")(app, passport);
     //favicon
     app.use('/favicon.ico', express.static('./webinterface/images/favicon.ico'));
 
