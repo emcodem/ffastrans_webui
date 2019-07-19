ec14705f74e3c935c2588e2bf23cf2234d51e4af
Harald Jordan <jordanh@jordanh-office-verylongname_test.local>
2019-07-19 09:48:57 +0200

    Fix + in filenames bug, Add User Permission FILTER_WORKFLOW_VARIABLES

--- node_components/views/getworkflowdetails.js
+++ node_components/views/getworkflowdetails.js
@@ -8,7 +8,7 @@ module.exports = function(app, passport){
 		try{
 			if (req.method === 'GET' || req.method === 'POST') {
                passport.authenticate('local-login');//fills req.user with infos from cookie
-               wf_id = req.body.name || req.query.name;
+               wf_id = req.body.workflowid || req.query.workflowid;
                if (!wf_id){
                    throw("Error, need workflowid parameter")
                }
@@ -17,7 +17,7 @@ module.exports = function(app, passport){
                //download workflowlist from ffastrans server
                 Request.get(buildApiUrl(global.config.STATIC_GET_WORKFLOWS_URL + "/" + wf_id), {timeout: 7000},(error, workflowResponse, body) => {
                     if(error) {
-                        global.socketio.emit("error", 'Error, webserver lost connection to ffastrans server. Is FFAStrans API online? ' + buildApiUrl(global.config.STATIC_GET_QUEUED_JOBS_URL));
+                        global.socketio.emit("error", 'Error, webserver lost connection to ffastrans server. Is FFAStrans API online? ' + buildApiUrl(global.config.STATIC_GET_WORKFLOWS_URL));
                         res.writeHead(200,{"Content-Type" : "text/text"});
                         res.write("");//output json array to client
                         res.end();
@@ -47,62 +47,56 @@ module.exports = function(app, passport){
                         
                    }
                    userpermissions.getpermissionlist(req.user["local"]["username"],function(allpermissions){
-                  
+                        
+                      // FILTER VARIABLES - this needs to be done in a to be generated getworkflow.js file and webui needs to use this url instead of proxy
                        
                        try{
                            for (x in allpermissions){
-                               
-                       //FILTER VARIABLES - this needs to be done in a to be generated getworkflow.js file and webui needs to use this url instead of proxy
-                                // try{
-                                       // if (allpermissions[x]["key"] == "FILTER_WORKFLOW_VARIABLES"){
-                                           // var filter = allpermissions[x]["value"]["filter"];
-                                           // console.log("VARIABLE FILTER ACTIVE: " + filter)
-                                           // for (var i in workflowlist["workflows/details"]){
-                                               // console.log(workflowlist["workflows/details"][i]["user_variables"]);
-                                               // for (var user_var_index in (workflowlist["workflows/details"][i]["user_variables"])){
+                                try{
+                                       //if we have a filter, iterate all variables of this workflow and filter them
+                                       var allowed_variable_array = [];
+                                       if (allpermissions[x]["key"] == "FILTER_WORKFLOW_VARIABLES"){
+                                           
+                                           console.log(req.user["local"]["username"] + " has got Filter permissions FILTER_WORKFLOW_VARIABLES: " );
+                                           console.log(allpermissions[x]["value"]);
+                                           var filter = allpermissions[x]["value"]["filter"];
+                                           console.log("VARIABLE FILTER ACTIVE: " + filter)
+                                           
+                                           for (var i in workflowlist["workflows"]){
+                                               console.log(workflowlist["workflows"][i]);
+                                               for (var user_var_index in (workflowlist["workflows"][i]["variables"])){
                                                    
-                                                   // var user_var = (workflowlist["workflows/details"][i]["user_variables"][user_var_index])
-                                                        // if (user_var["name"].toLowerCase().match(filter.toLowerCase())){
-                                                             // //allow
-                                                             // console.log("Matched allowed User variable: " + user_var["name"])
-                                                        // }else{
-                                                            // console.log("Hiding User variable due to filter settings: " + user_var["name"])
-                                                            // workflowlist["workflows/details"][i]["user_variables"] = [];
-                                                            // console.log(workflowlist["workflows/details"][i]["user_variables"])
-                                                            
-                                                        // }
-                                                   // }
-                                               // }
-                                           // }
-                                   // }
-                                   // catch(exec){
-                                       // console.error("Error parsing user variables from workflows (contact developer): " + exec);
-                                   // }
+                                                   var user_var = (workflowlist["workflows"][i]["variables"][user_var_index])
+                                                        if (user_var["name"].toLowerCase().match(filter.toLowerCase())){
+                                                             //allow
+                                                             console.log("Matched allowed User variable: " + user_var["name"])
+                                                             allowed_variable_array.push(user_var)
+                                                        }else{
+                                                            console.log("Hiding User variable due to filter settings: " + user_var["name"])
+                                                        }
+                                                   }
+                                               }//for all workflows
+                                            //finally update workflowlist with new allowed variable array 
+                                       }
+                                       workflowlist["workflows"][i]["variables"] = allowed_variable_array;
+                                   }
+                                   catch(exec){
+                                       console.error("Error parsing user variables from workflows (contact developer): " + exec);
+                                   }
                                
                        
                        
                            }//for allpermissions
                        }catch(ex){
-                            console.log("ERROR: error in getworkflowlist: " + ex);
+                            console.log("ERROR: error in getworkflow variables: " + ex);
                             res.status(500);//Send error response here
                             res.end();
                             return;
                        }
 
-                       //output all worfklows if something is wrong
-                       if (filteredWorkflowList.length == 0){
-                           //output all workflows
-                            console.warn("Workflow Filter for user " + req.user["local"]["username"] + " returned 0 workflows, serving all workflows");
-                            console.warn(filteredWorkflowList)
-                            res.writeHead(200,{"Content-Type" : "application/JSON"});
-                            res.write(JSON.stringify(workflowlist));//output json array to client
-                            res.end();
-                            return;
-                       }
-                       
                        //finally output filtered list 
-                        console.log("serving filtered workflow list")
-                        workflowlist["workflows/details"] = filteredWorkflowList;
+                        console.log("serving filtered workflow variables list")
+                        //workflowlist["workflows/details"] = filteredWorkflowList;
                         res.writeHead(200,{"Content-Type" : "application/JSON"});
                         res.write(JSON.stringify(workflowlist));//output json array to client
                         res.end();
@@ -124,18 +118,6 @@ function buildApiUrl(what){
     return "http://" + global.config.STATIC_API_HOST + ":" +  global.config.STATIC_API_PORT + what;  
 }
 
-function hashCode (string) {
-//this creates a hash from a stringified object, it is used to workaround and create missing jobids from ffastrans version 0.9.3
-  var hash = 0, i, chr;
-  if (string.length === 0) return hash;
-  for (i = 0; i < string.length; i++) {
-    chr   = string.charCodeAt(i);
-    hash  = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
-};
-  
   
   
   
