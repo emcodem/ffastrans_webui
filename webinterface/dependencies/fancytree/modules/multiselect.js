9f64c5937f63c8809bed83fce8e2701aa2d951a2
emcodem <harald.jordan78+1@gmail.com>
2019-11-16 10:28:35 +0100

    logviewer

---
+++ webinterface/dependencies/fancytree/modules/multiselect.js
@@ -0,0 +1,123 @@
+/*
+ * Jquery Multiselect插件 中文叫列表多选插件
+ * 使用例子:
+ * $('table').multiSelect({
+ *  actcls: 'active',
+ *  selector: 'tbody tr',
+ *  callback: false
+ * });
+ */
+(function ($) {
+    $.fn.multiSelect = function (options) {
+        $.fn.multiSelect.init($(this), options);
+    };
+
+    $.extend($.fn.multiSelect, {
+        defaults: {
+            actcls: 'active', //选中样式
+            selector: 'tbody tr', //选择的行元素
+            except: ['tbody'], //选中后不去除多选效果的元素队列
+            statics: ['.static'], //被排除行元素条件
+            callback: false //选中回调
+        },
+        first: null, //按shift时，用于记住第一个点击的item
+        last: null, //最后点击的item
+        init: function (scope, options) {
+            this.scope = scope;
+            this.options = $.extend({}, this.defaults, options);
+            this.initEvent();
+        },
+        checkStatics: function (dom) {
+            for (var i in this.options.statics) {
+                if (dom.is(this.options.statics[i])) {
+                    return true;
+                }
+            }
+        },
+        initEvent: function () {
+            var self = this,
+                scope = self.scope,
+                options = self.options,
+                callback = options.callback,
+                actcls = options.actcls;
+
+            scope.on('click.mSelect', options.selector, function (e) {
+                if (!e.shiftKey && self.checkStatics($(this))) {
+                    return;
+                }
+
+                if ($(this).hasClass(actcls)) {
+                    $(this).removeClass(actcls);
+                } else {
+                    $(this).addClass(actcls);
+                }
+
+                if (e.shiftKey && self.last) {
+                    if (!self.first) {
+                        self.first = self.last;
+                    }
+                    var start = self.first.index();
+                    var end = $(this).index();
+                    if (start > end) {
+                        var temp = start;
+                        start = end;
+                        end = temp;
+                    }
+                    $(options.selector, scope).removeClass(actcls).slice(start, end + 1).each(function () {
+                        if (!self.checkStatics($(this))) {
+                            $(this).addClass(actcls);
+                        }
+                    });
+                    window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
+                } else if (!e.ctrlKey && !e.metaKey) {
+                    $(this).siblings().removeClass(actcls);
+                }
+                self.last = $(this);
+                $.isFunction(callback) && callback($(options.selector + '.' + actcls, scope));
+            });
+
+            /**
+             * 点击其他处去除选中状态
+             */
+            $(document).on('click.mSelect', function (e) {
+                for (var i in options.except) {
+                    var except = options.except[i];
+                    if ($(e.target).is(except) || $(e.target).parents(except).size()) {
+                        return;
+                    }
+                }
+                scope.find(options.selector).each(function () {
+                    if (!self.checkStatics($(this))) {
+                        $(this).removeClass(actcls);
+                    }
+                });
+                $.isFunction(callback) && callback($(options.selector + '.' + actcls, scope));
+            });
+
+            /**
+             * Ctrl+A全选
+             */
+            $(document).on('keydown.mSelect', function (e) {
+                if ((e.keyCode == 65) && (e.metaKey || e.ctrlKey)) {
+                    $(options.selector, scope).each(function () {
+                        if (!self.checkStatics($(this))) {
+                            $(this).addClass(actcls);
+                        }
+                    });
+                    $.isFunction(callback) && callback($(options.selector + '.' + actcls, scope));
+                    e.preventDefault();
+                    return false;
+                }
+            });
+
+            /**
+             * 清楚shift按住的状态
+             */
+            $(document).on('keyup.mSelect', function (e) {
+                if (e.keyCode == 16) {
+                    self.first = null;
+                }
+            });
+        }
+    });
+})(jQuery);
---
+++ webinterface/dependencies/json_viewer/README.md
@@ -0,0 +1,58 @@
+# jQuery json-viewer
+
+![npm](https://img.shields.io/npm/v/jquery.json-viewer.svg)
+![npm](https://img.shields.io/npm/l/jquery.json-viewer.svg)
+![npm](https://img.shields.io/npm/dt/jquery.json-viewer.svg)
+
+*json-viewer* is a jQuery plugin for easily displaying JSON objects by transforming them into HTML.
+
+Features:
+- Syntax highlighting
+- Collapse and expand child nodes on click
+- Clickable links
+- Easily readable and minimal DOM structure
+
+Check out the [demo page](http://rawgit.com/abodelot/jquery.json-viewer/master/demo.html)!
+
+## Usage
+
+Import `jquery.json-viewer.js` and `jquery.json-viewer.css` in your application.
+
+Copy sources from git repository, or use npm:
+
+```sh
+npm install jquery.json-viewer
+```
+
+Then just call the `jsonViewer()` method and pass your JSON data in argument:
+```html
+<pre id="json-renderer"></pre>
+```
+
+```js
+var data = {
+  "foobar": "foobaz"
+};
+$('#json-renderer').jsonViewer(data);
+```
+
+## Options
+
+The `jsonViewer` method accepts an optional config object as a second argument. The supported options are:
+
+- `collapsed` (boolean, default: `false`): all nodes are collapsed at html generation.
+- `rootCollapsable` (boolean, default: `true`): allow root element to be collasped.
+- `withQuotes` (boolean, default: `false`): all JSON keys are surrounded with double quotation marks (`{"foobar": 1}` instead of `{foobar: 1}`).
+- `withLinks` (boolean, default: `true`): all values that are valid links will be clickable, if `false` they will only be strings.
+
+Example:
+
+```js
+$('#json-renderer').jsonViewer(data, {collapsed: true, withQuotes: true, withLinks: false});
+```
+
+## About
+
+- Author: [Alexandre Bodelot](https://github.com/abodelot)
+- License: [MIT License](http://opensource.org/licenses/MIT)
+
