1f9acdc13d9be9350a8f5f4b40449b3b509d67df
emcodem <emcodem@github.com>
2020-11-11 17:33:29 +0100

    work on pending jobs

--- rest_service/api/controllers/tickets.js
+++ rest_service/api/controllers/tickets.js
@@ -14,34 +14,64 @@ async function get_pending(returnarray){
 		//enrich workflow names for pending tickets
 		var found_incoming = [];
 		 await recursive(s_monitor_path).forEach(function(fname){
-			if (fname.indexOf("grow") != -1){ 
-			if (fname.indexOf("/i/") != -1){
-				returnarray.push(fname);
-			}else{
-				console.log(fname,"not included")
-			}
-			}
-		 })
+            
+            try{
+                if (fname.indexOf("\\i\\") != -1){
+                    var newitem = {};
+                    var fullpath = path.join (s_monitor_path,fname);
+                    //read the monitor incoming record to get the filename of interest
+                    var _readout = (JSON.parse(fs.readFileSync(fullpath, 'utf8').replace(/^\uFEFF/, '')))//removes BOM	;
+                    
+                    newitem["host"] = _readout["host"]
+                    var _realfile = _readout["source"];
+                    
+                    var myRegexp = /(........-....-....-....-............).*?mons/gi;
+                    var _matches =  myRegexp.exec(fullpath);
+                    var ctime = fs.statSync(fullpath).birthtime;
+                    
+                    //console.log("parsed wfid: ",_matches[1])
+                    newitem["fullpath"] = _realfile;
+                    newitem["sources"] = {"current_file": _realfile};
+                    newitem["submit"] = {"time":ctime};
+                    newitem["nodes"] = {"next":"Watchfolder","type":"Watchfolder"};
+                    newitem["internal_wf_name"] = get_wf_name(_matches[1]);
+//                    newitem["internal_wf_id"] = 
+                    //console.log("pending:",newitem)
+                    found_incoming.push(newitem);
+                }else{
+                    
+                }
+                
+            }catch(ex){
+                console.log(ex)
+                
+            }
+			
+		 });
+         return found_incoming;
             // for (var x=0;x<returnarray.length;x++){
                 
-                // try{
-                    // var workflow_folder = path.join(global.api_config["s_SYS_CACHE_DIR"],"../configs/workflows/");
-                    // var wf_path = path.join(workflow_folder,returnarray[x]["internal_wf_id"]) + ".json";
-                    // //console.log("Reading worfklow for pending ticket: ", wf_path)
-                    // var wf_obj = fs.readFileSync(wf_path, 'utf8' ).replace(/^\uFEFF/, '');
-                    // wf_obj = JSON.parse(wf_obj);
-                    // returnarray[x]["workflow"] = wf_obj["wf_name"];
-                    // //console.log("Found workflow name: ", wf_obj["wf_name"])
-                    
-                // }catch(ex){
-                    // console.error("Error reading workflow for pending ticket: ",ex);
-                    
-                // }
+
             // }
         
 	return [];
 }
 
+function get_wf_name(wf_id){
+        try{
+            var workflow_folder = path.join(global.api_config["s_SYS_CACHE_DIR"],"../configs/workflows/");
+            var wf_path = path.join(workflow_folder,wf_id) + ".json";
+            //console.log("Reading worfklow for pending ticket: ", wf_path)
+            var wf_obj = fs.readFileSync(wf_path, 'utf8' ).replace(/^\uFEFF/, '');
+            wf_obj = JSON.parse(wf_obj);
+            return wf_obj["wf_name"];
+           
+        }catch(ex){
+            console.error("Error reading workflow for pending ticket: ",ex);
+            
+        }
+    
+}
 //all files in all directories to array
 function jsonfiles_to_array(dir) {
 
@@ -78,7 +108,6 @@ async function start(req, res) {
 		o_return["tickets"]["queue"] = jsonfiles_to_array(path.join(s_tick_path,"queue"));
 		o_return["tickets"]["pending"] = await get_pending();
 		
-		console.log("pending" ,o_return["tickets"]["pending"])
 		res.json(o_return);
 		res.end();
 	} catch(err) {
