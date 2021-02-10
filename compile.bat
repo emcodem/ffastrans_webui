39d626956f0b6a7f7926390d7fb8f84c2fdfb2e6
emcodem <emcodem@ffastrans.com>
2021-02-10 17:56:50 +0100

    update compile to newer node version

--- compile.bat
+++ compile.bat
@@ -1,2 +1,3 @@
+REM DOWNLOAD PREBUILT NEXE NODE BINS for your version FROM https://github.com/nexe/nexe/releases/
 cd "%~dp0"
-nexe server.js -t windows-x64-12.14.0 -r "./webinterface/**" -r "./rest_service/**" 
+nexe server.js -t windows-x64-14.15.3 -r "./webinterface/**" -r "./rest_service/**" 
