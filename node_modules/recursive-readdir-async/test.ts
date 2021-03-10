ef8a85644f6f504ea1122d2fe5faa331f7c96283
emcodem <emcodem@github.com>
2021-03-10 13:03:41 +0100

    minimize release size

--- node_modules/recursive-readdir-async/test.ts
+++
@@ -1,7 +0,0 @@
-import * as rra from './build/module.esm'
-
-console.log('start')
-rra.list('./test').then(data => {
-    console.log(data)
-})
-console.log('end')
--- rest_service/node_modules/fs-routes/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface FsRoutesOptions {
-    glob?: string;
-    indexFileRegExp?: RegExp;
-}
-export interface FsRoute {
-    path: string;
-    route: string;
-}
-export default function fsRoutes(dir: string, options?: FsRoutesOptions): FsRoute[];
