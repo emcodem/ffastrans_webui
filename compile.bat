715b96db77208b6e378aaa9ab60c39ebe3f1de9d
emcodem <emcodem@ffastrans.com>
2021-03-10 14:21:44 +0100

    minimized release

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
 REM DOWNLOAD PREBUILT NEXE NODE BINS for your version FROM https://github.com/nexe/nexe/releases/
 cd "%~dp0"
-nexe server.js -t windows-x64-14.15.3 -r "./webinterface/**" -r "./rest_service/*/!(rabbitmq-server)**" 
+REM nexe server.js -t windows-x64-14.15.3 -r "./webinterface/**" -r "./rest_service/*/{*,!(rabbitmq-server)/**}" -r  "./rest_service/app.js" --verbose
+nexe server.js -t windows-x64-14.15.3 -r "./webinterface/**" -r "./rest_service/**" -r  "./rest_service/app.js" --verbose
