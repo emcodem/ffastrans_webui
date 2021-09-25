ddab8fa835b413f62cd08c7417aeb82f48e3f167
emcodem <emcodem@ffastrans.com>
2021-09-25 22:40:12 +0200

    initial 1.3 workflows folder compatibility

--- rest_service/api/controllers/get_job_details.js
+++ rest_service/api/controllers/get_job_details.js
@@ -49,7 +49,33 @@ function start(req, res) {
                         respond((o_job));
                         return;
                     });
-                }else {
+                }else if (fs.existsSync(global.api_config["s_SYS_JOB_DIR"] + jobid + "/workflows")){
+                    var wf_path = global.api_config["s_SYS_JOB_DIR"] + jobid + "/workflows";
+                    console.log("workflows folder found in ",global.api_config["s_SYS_JOB_DIR"],",assuming ffastrans Version 1.3");
+                    var files = fs.readdirSync(wf_path);
+                    if (files.length < 1){
+                        console.error("Expected at least 1 file in ",wf_path);
+                        res_error("Expected at least 1 file in ",wf_path);
+                        return;
+                    }
+                    //serve first found workflow file. TODO: for 1.3, check if we can have more than 1 workflow here
+                    fs.readFile(wf_path + "/" + files[0], 'utf8', function (err, wf_file_contents) {
+                        if (err) {
+                            console.error(err);
+                            res_error();
+                            return;
+                        }
+                        wf_file_contents = wf_file_contents.trim();
+                        o_job["wf_object"] = JSON.parse(wf_file_contents);
+                        console.debug("responding with wf_object")
+                        respond(o_job);
+                        return;
+                    });
+                    return;
+                }
+                
+                else {
+                    console.log("no workflow json found, trying ",global.api_config["s_SYS_WORKFLOW_DIR"] + "workflow.json")
                     try {
                         var wf_id = JSON.parse(job_file_contents)["workflow"]["id"];
                         if (fs.existsSync(global.api_config["s_SYS_WORKFLOW_DIR"] + "workflow.json")) {
@@ -62,10 +88,12 @@ function start(req, res) {
                         }
                     } catch (ex) {
                         console.error("Error getting Workflow for jobid: " + jobid);
+                        res_error("Unexpected error getting get_job_details");
+                        return;
                     }
                 }
-              console.debug("get_job_details serving " + job_file_contents.length + " bytes");
-
+              console.error("Unexpected error getting get_job_details serving " + job_file_contents.length + " bytes");
+              res_error("Unexpected error getting get_job_details");
           });
         
             function res_error() {
