0f3c932a5edb47291bcd292fd7d1734c6c4131f7
emcodem <emcodem@ffastrans.com>
2023-08-29 11:00:31 +0200

    del old grafana

--- tools/prometheus/grafana-7.4.3.old/public/app/plugins/datasource/elasticsearch/components/QueryEditor/BucketAggregationsEditor/SettingsEditor/FiltersSettingsEditor/utils.ts
+++
@@ -1,3 +0,0 @@
-import { Filter } from '../../aggregations';
-
-export const defaultFilter = (): Filter => ({ label: '', query: '*' });
--- tools/prometheus/grafana-7.4.3.old/public/app/plugins/datasource/elasticsearch/components/QueryEditor/styles.ts
+++
@@ -1,5 +0,0 @@
-import { css } from 'emotion';
-
-export const segmentStyles = css`
-  min-width: 150px;
-`;
