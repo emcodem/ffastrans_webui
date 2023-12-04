be68f6e76a9ba7300cefa6f489091dc323261104
harry <harald.jordan78@gmail.com>
2023-12-04 03:01:04 +0100

    build resources

--- compile.bat
+++ compile.bat
@@ -11,7 +11,13 @@ echo %MAJOR%.%MINOR%.%NEWVERSION% > "%~dp0/webinterface/version.txt"
 cmd /C "ncc build server.js -o dist"
 
 REM WORKS cmd /C "nexe /dist/index.js  -t windows-x64-18.14.0 -r ./webinterface/** -r ./rest_service/** -r  ./rest_service/app.js" --remote https://github.com/urbdyn/nexe_builds/releases/download/0.3.0/
-cmd /C "nexe /dist/index.js  -t windows-x64-18.14.0 -r ./webinterface/**  -r "./rest_service/**" -r  "./rest_service/app.js" --remote https://github.com/urbdyn/nexe_builds/releases/download/0.3.0/
+cmd /C "nexe /dist/index.js -t windows-x64-18.14.0 -r ./webinterface/**  -r "./rest_service/**" -r  "./rest_service/app.js" --verbose --ico "%~dp0build_tools/webint_icon.ico""
+
+cmd /C "%~dp0build_tools/verpatch.exe server.exe "%MAJOR%.%MINOR%.%NEWVERSION%.0"  /s product "FFAStrans Webinterface" /s desc "Node.js 18.14.0 is embedded" /s copyright "emcodem@FFAStrans.com" "
+
+cmd /C ""%~dp0build_tools/ResourceHacker.exe" -open "server.exe" -save "server.exe" -action addoverwrite -resource "%~dp0build_tools\webint_icon.ico" -mask ICONGROUP,MAINICON"
+ 
+ie4uinit.exe -ClearIconCache
 
 REM cmd /C "nexe test.js  -t windows-x64-18.14.0 --remote https://github.com/urbdyn/nexe_builds/releases/download/0.3.0/
 
