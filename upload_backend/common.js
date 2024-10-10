c67e8be1ae89160314648029c62d1609a3ffc2e3
emcodem <emcodem@ffastrans.com>
2024-10-10 16:33:34 +0200

    audit update busboy

--- upload_backend/common.js
+++ upload_backend/common.js
@@ -1,5 +1,6 @@
 const Busboy = require('busboy');
 const fs = require('fs');
+const path = require('path');
 
 module.exports = function(app, express){
 
@@ -8,19 +9,19 @@ module.exports = function(app, express){
 
 	//download files - not yet supported nor used
 	app.use('/backend/upload/files/:file', (req, res) => {
-		var file = config.STATIC_UPLOADPATH +req.params.file;
+		var file = global.config.STATIC_UPLOADPATH +req.params.file;
 		res.download(file);
 	});
 
 	//upload files
 	app.post('/backend/upload', (req, res) => {
 		if (req.method === 'POST') {
-			const busboy = new Busboy({ headers: req.headers });
+			const busboy = Busboy({ headers: req.headers });
 			const response = {};
-			busboy.on('file', (fieldname, file, filename) => {
-				console.log(fieldname, filename);
-				const saveTo = config.STATIC_UPLOADPATH + filename;
-				response.link = `${filename}`;
+			busboy.on('file', (fieldname, file, o_fileinfo) => {
+				console.log(fieldname, o_fileinfo.filename);
+				const saveTo = path.join(global.config.STATIC_UPLOADPATH, o_fileinfo.filename);
+				response.link = `${o_fileinfo.filename}`;
 
 				file.pipe(fs.createWriteStream(saveTo));
 			});
