c50b50c81b477bf57300a8e90644506d462bd84e
emcodem <emcodem@ffastrans.com>
2024-12-16 23:05:07 +0100

    fix placement of resizer icons

--- upload_backend/common.js
+++ upload_backend/common.js
@@ -1,40 +1,41 @@
-const Busboy = require('busboy');
-const fs = require('fs');
-const path = require('path');
+//disabled since TUS
+// const Busboy = require('busboy');
+// const fs = require('fs');
+// const path = require('path');
 
-module.exports = function(app, express){
+// module.exports = function(app, express){
 
-	//serve previews - not yet supported nor used
-	app.use("/backend/upload/previews", express.static(__dirname + '/previews'));
+// 	//serve previews - not yet supported nor used
+// 	app.use("/backend/upload/previews", express.static(__dirname + '/previews'));
 
-	//download files - not yet supported nor used
-	app.use('/backend/upload/files/:file', (req, res) => {
-		var file = path.join(global.config.STATIC_UPLOADPATH +req.params.file);
-		res.download(file);
-	});
+// 	//download files - not yet supported nor used
+// 	app.use('/backend/upload/files/:file', (req, res) => {
+// 		var file = path.join(global.config.STATIC_UPLOADPATH +req.params.file);
+// 		res.download(file);
+// 	});
 
-	//upload files
-	app.post('/backend/upload', (req, res) => {
-		if (req.method === 'POST') {
-			const busboy = Busboy({ headers: req.headers, defCharset:'utf8',defParamCharset: 'utf8'});
-			const response = {};
-			busboy.on('file', (fieldname, file, o_fileinfo) => {
-				console.log(fieldname, o_fileinfo.filename);
-				const saveTo = path.join(global.config.STATIC_UPLOADPATH, o_fileinfo.filename);
-				response.link = `${o_fileinfo.filename}`;
-				console.log("Upload file path: ",saveTo);
-				file.pipe(fs.createWriteStream(saveTo));
-			});
-			busboy.on('field', (fieldname, val) => {
-				console.log("Uploaded File:" + fieldname + ': '+ val);
-			});
-			busboy.on('finish', () => {
-				res.json(response);
-			})
-			return req.pipe(busboy);
-		}
-		res.writeHead(404);
-		res.end();
-	});
+// 	//upload files
+// 	app.post('/backend/upload', (req, res) => {
+// 		if (req.method === 'POST') {
+// 			const busboy = Busboy({ headers: req.headers, defCharset:'utf8',defParamCharset: 'utf8'});
+// 			const response = {};
+// 			busboy.on('file', (fieldname, file, o_fileinfo) => {
+// 				console.log(fieldname, o_fileinfo.filename);
+// 				const saveTo = path.join(global.config.STATIC_UPLOADPATH, o_fileinfo.filename);
+// 				response.link = `${o_fileinfo.filename}`;
+// 				console.log("Upload file path: ",saveTo);
+// 				file.pipe(fs.createWriteStream(saveTo));
+// 			});
+// 			busboy.on('field', (fieldname, val) => {
+// 				console.log("Uploaded File:" + fieldname + ': '+ val);
+// 			});
+// 			busboy.on('finish', () => {
+// 				res.json(response);
+// 			})
+// 			return req.pipe(busboy);
+// 		}
+// 		res.writeHead(404);
+// 		res.end();
+// 	});
 
-}
+// }
